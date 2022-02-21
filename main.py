15d7249b77225274b398354eb9544f220e0d42a2
HarisBegi <70474327+harisbegic2001@users.noreply.github.com>
2022-02-21 10:21:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+                        #DAY 12
+import random as r
+
+
+def logic(zivoti):
+        while zivoti > 0:
+            print(f"You have {zivoti} lives")
+            answer = int(input("Enter your answer: "))
+
+            if answer == correct_answer:
+                print("YOU WON!")
+                break
+            
+            elif answer < correct_answer:
+                print("Too low")
+                zivoti -= 1
+                if zivoti > 0:
+                    print("Guess Again")
+                else:
+                    print("You lost :(")
+
+            elif answer > correct_answer:
+                print("Too high")
+                zivoti -= 1
+                if zivoti > 0:
+                    print("Guess Again")
+                else:
+                    print("You lost :(")
+            
+
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+
+correct_answer = r.randint(1,100)
+
+difficulty = input("Choose difficulty \"easy\" or \"hard\": ").lower()
+
+if difficulty == "easy":
+    lives = 10
+    logic(lives)
+
+elif difficulty == "hard":
+    lives = 5
+    logic(lives)
